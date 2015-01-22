ea5c1c7649f3d2bc253ecef6f4d681c9535782c7
MKrabbenhoeft <qq@q>
2015-01-22 10:11:02 +0100

    Back up Commit

--- src/robowarrior/BulletAvoidBot.java
+++ src/robowarrior/BulletAvoidBot.java
@@ -6,11 +6,14 @@ import robocode.ScannedRobotEvent;
 import robocode.StatusEvent;
 import robowarrior.core.Bots.EnemyBot;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.lang.management.OperatingSystemMXBean;
 import java.util.ArrayList;
 
 import static robocode.util.Utils.normalRelativeAngleDegrees;
+import static robowarrior.core.Utils.MapUtils.getRigthWinkel;
 import static robowarrior.core.Utils.MathUtils.getCoords;
 
 public class BulletAvoidBot extends AdvancedRobot {
@@ -43,7 +46,7 @@ public class BulletAvoidBot extends AdvancedRobot {
         if (changeInEnergy > 0 && changeInEnergy <= 3) {
             if(!rect.contains(getX(),getY())){
                 out.println(getX()+" | "+getY());
-                movementDirection = -movementDirection;
+                movementDirection = -movementDirection; nn
 
             }
             movementDirection = -movementDirection;
@@ -52,9 +55,12 @@ public class BulletAvoidBot extends AdvancedRobot {
 
            double[] pos = getCoords(0,Dis,this.getHeading(), getX(), getY());
 
-
-            setAhead((event.getDistance() / 4 + 25) * movementDirection);
-
+            if(!rect.contains(pos[0],pos[1])){
+               goTo(new Point2D.Double(getBattleFieldWidth() / 2, getBattleFieldHeight() / 2)) ;
+            }
+            else {
+                setAhead((event.getDistance() / 4 + 25) * movementDirection);
+            }
 
         }
     }
@@ -90,4 +96,44 @@ public class BulletAvoidBot extends AdvancedRobot {
 
         }
     }
+
+    private void goTo( Point2D NextPoint) {
+        // Methode damit ich mich zu meinen Nächsten Punkt fahre....
+
+        // Distance & Winkel holen
+        double distance = this.location().distance(NextPoint);
+        double angle = normalRelativeAngle(absoluteBearing(location(), NextPoint) - getHeading());
+        // Distance und Winkel anpassen
+        double[] aangle = getRigthWinkel(angle, distance);
+
+        // Drehen und fahren !
+        setTurnRight(aangle[0]);
+        setAhead(aangle[1]);
+    }
+
+    private Point2D location() {
+        // Punkt erstellen mit meinen koordinaten & zurück geben
+        return new Point2D.Double(getX(), getY());
+    }
+
+    private double absoluteBearing(Point2D source, Point2D target) {
+        // WInkelerrechnen ( in Bogenmaß) , mithilfe der Tan funktionen, aus minen Punkt und miene Wunsch punk
+        return Math.toDegrees(Math.atan2(target.getX() - source.getX(), target.getY() - source.getY()));
+    }
+
+    private double normalRelativeAngle(double angle) {
+        // winkel ...
+        // mit den Parameter,
+        // modullieren...
+        //  und irgendwas irgendwie mit den Winkeln machen ... ? O.o
+
+        double relativeAngle = angle % 360;
+        if (relativeAngle <= -180)
+            return 180 + (relativeAngle % 180);
+        else if (relativeAngle > 180)
+            return -180 + (relativeAngle % 180);
+        else
+            return relativeAngle;
+    }
+
 }
